d3cf36e52ca54a25a92891ca0ed5e956681b9d6d
BRivsC <104479155+BRivsC@users.noreply.github.com>
2022-05-03 18:59:14 -0400

    Add files via upload

---
+++ GVI05_Sesion4/Act1/S4_actividad1.sv
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// University: Universidad Tecnica Federico Santa Maria
+// Course: ELO212
+// Students: 
+// 
+// Create Date: 
+// Design Name: Guia 4
+// Module Name: S4_actividad1
+// Project Name: 
+// Target Devices: xc7a100tcsg324-1
+// Tool Versions: Vivado 2021.1
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module S4_actividad1(
+    input  logic        clock,
+    input  logic        reset,
+    input  logic [31:0] BCD_in,
+    output logic [6:0]  segments,
+    output logic [7:0]  anodos
+    );
+    
+    
+    
+endmodule
