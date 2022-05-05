6a4b328a35b970864c8dca0bfdab2eed71a4bc4a
BRivsC <104479155+BRivsC@users.noreply.github.com>
2022-05-05 01:13:25 -0400

    Add files via upload

--- GVI05_Sesion4/Act3/S4_actividad3.sv
+++ GVI05_Sesion4/Act3/S4_actividad3.sv
@@ -2,21 +2,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 // University: Universidad Tecnica Federico Santa Maria
 // Course: ELO212
-// Students: 
+// Students: Cristobal Caqueo, Bastian Rivas, Claudio Zanetta
 // 
-// Create Date: 
+// Create Date: 04/05/2022
 // Design Name: Guia 4
 // Module Name: S4_actividad3
-// Project Name: 
+// Project Name: n_bit_ALU
 // Target Devices: xc7a100tcsg324-1
 // Tool Versions: Vivado 2021.1
-// Description: 
+// Description: Unidad ALU que suma, resta, aplica OR y AND
 // 
-// Dependencies: 
+// Dependencies: Lab Digitales
 // 
-// Revision:
+// Revision: 0.01
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: Por defecto trabaja con 8 bits
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -26,8 +26,89 @@ module S4_actividad3 #(parameter M = 8)(
     input  logic [1:0]      OpCode,       
     output logic [M-1:0]    Result,
     output logic [3:0]      Status
+    //output logic[M:0] resultado //pa verla en la sim
     );
     
-    
+ 	logic[M:0] resultado; 
+
+	always_comb begin
+	Result=0;  //valores iniciales para evitar latches
+	Status=0;  //no latches?
+		if ( OpCode == 2'b00 ) begin							//OpCode = 00, se suma.
+			resultado = A + B;
+			Result = resultado [M-1:0];						//Se crea una señal resultado, con un bit extra, para identificar cuando hay overflow
+			if ( Result == 0 ) begin
+				Status[2] = 1;
+			end else begin
+				Status[2] = 0;
+			end if ( Result[M-1] == 1 ) begin					//Si se está trabajando con complemento de 2, el bit más significativo es el bit del signo, y si este es 1, el número es negativo. Corresponde al usuario interpretar este resultado
+				Status[3] = 1;
+			end else begin
+				Status[3] = 0;
+			end if ( resultado[M] == 1 ) begin					//Si el bit más significativo de resultado es 1, quiere decir que hubo overflow.
+				Status[1] = 1;
+			end else begin
+				Status[0] = 0;
+			end if ( A[M-1] == 0 && B[M-1] == 0 && Result[M-1] == 1 ) begin		//Si se a un número positivo (negativo) se le suma un número positivo (negativo), y el resultado es negativo (positivo), el resultado excedió la capacidad de representación.
+				Status[0] = 1;
+			end else if ( A[M-1] == 1 && B[M-1] == 1 && Result[M-1] == 0 ) begin
+				Status[0] = 1;
+			end else begin
+				Status[0] = 0;
+			end
+		end
+		if ( OpCode == 2'b01 ) begin								//Si OpCode es 01, se resta A y B.
+			resultado = A - B;
+			Result = resultado[M-1:0];
+			if ( Result == 0 ) begin
+				Status[2] = 1;
+			end else begin
+				Status[2] = 0;
+		    end
+			if ( Result[M-1] == 1) begin
+				Status[3] = 1;
+			end else begin
+				Status[3] = 0;
+			end if (resultado[M] == 1) begin
+				Status[1] = 1;
+			end else begin
+				Status[1] = 0;
+			end if ( A[M-1] == 1 && B[M-1] == 0 && Result[M-1] == 0 ) begin		//Si a un número positivo (negativo) se le resta un número negativo (positivo), y el resultado es negativo (positivo), el resultado excedió la capacidad de representación.
+				Status[0] = 1;
+			end else if ( A[M-1] == 0 && B[M-1] == 1 && Result[M-1] == 1 ) begin
+				Status[0] = 1;
+			end else begin
+				Status[0] = 0;
+			end
+		end
+		if ( OpCode == 2'b10 ) begin								//Si OpCode es 10, se hace el OR entre A y B.
+			Result = A | B;	
+			Status[0] = 0;									//C y V permanecen apagados.
+			Status[1] = 0;
+			if ( Result == 'd0 ) begin
+				Status[2] = 1;
+			end else begin
+				Status[2] = 0;
+			end if ( Result[M-1] == 1 ) begin
+				Status[3] = 1;
+			end else begin
+				Status[3] = 0;
+		    end
+		end
+		if ( OpCode == 2'b11 ) begin								//Si OpCode es 11, se hace el AND entre A y B.
+			Result = A & B;								
+			Status[0] = 0;									//C y V permanecen apagados
+			Status[1] = 0;
+			if ( Result == 'd0 ) begin
+				Status[2] = 1;
+			end else begin
+				Status[2] = 0;
+			end if ( Result[M-1] == 1 ) begin
+				Status[3] = 1;
+			end else begin
+				Status[3] = 0;
+			end
+		end
+	end   
     
 endmodule
---
+++ GVI05_Sesion4/Act3/test_ALU.sv
@@ -0,0 +1,115 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// University: Universidad Tecnica Federico Santa Maria
+// Course: ELO212
+// Students: Cristobal Caqueo, Bastian Rivas, Claudio Zanetta
+// 
+// Create Date: 04/05/2022
+// Design Name: Guia 4
+// Module Name: test_ALU
+// Project Name: n_bit_ALU
+// Target Devices: xc7a100tcsg324-1
+// Tool Versions: Vivado 2021.1
+// Description: Testbench que prueba el funcionamiento normal y de cada flag posible 
+//              de la ALU
+// 
+// Dependencies: Lab Digitales
+// 
+// Revision: 0.01
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_test();
+	logic [7:0] A,B,Result;
+	logic [1:0] OpCode;
+	logic [3:0] Status;
+	logic [8:0] resultado;
+	
+	S4_actividad3 DUT(
+		.A(A),
+		.B(B),
+		.OpCode(OpCode),
+		.Result(Result),
+		.Status(Status)/*,
+		.resultado(resultado)*/
+		);
+	
+	initial begin
+	    //Caso 1
+		OpCode = 'd0;			//Suma
+		A = 8'b11111111;		
+		B = 8'b11111111;		//Se está probando el flag C, overflow
+	
+		#10
+        //Caso 2
+		A = 8'b01111111;	
+		B = 8'b01111111;		//Se está probando el flag V, suma de números positivos que da un número negativo
+	
+		#10
+	    //Caso 3
+		A = 8'b10011001;
+		B = 8'b11011000;		//Se está probando el flag V, suma de números negativos que da un número positivo
+
+		#10
+        //Caso 4
+		A = 8'b01110110;
+		B = 8'b10001010;			//Se está probando el flag Z, que el resultado sea 0 
+
+		#10
+        //Caso 5
+		A = 8'b10011011;
+		B = 8'b00000111;			//Se está probando el flag N, que el resultado sea negativo
+
+		#10
+        //Caso 6
+		OpCode = 'd1;			//Resta
+		A = 8'b00010110;
+		B = 8'b01011000;			//Se está probando el flag C, borrow
+
+		#10
+        //Caso 7
+		A = 8'b01100100;
+		B = 8'b11011000;			//Se está probando el flag V, restarle un número negativo a un positivo y obtener un negativo
+
+		#10
+        //Caso 8
+		A = 8'b10001111;
+		B = 8'b00101000;			//Se está probando el flag V, restarle un número positivo a un negativo y obtener un positivo
+
+		#10
+        //Caso 9
+		A = 8'b01110100;
+		B = 8'b01111100;			//Se está probando el flag N, que el resultado sea negativo
+
+		#10
+        //Caso 10
+		A = 8'b11111111;
+		B = 8'b11111111;			//Se está probando el flag Z, que el resultado sea 0
+
+		#10
+        //Caso 11
+		OpCode = 'd2;			//OR
+		A = 8'b11001001;			//C y V deben ser 0
+		B = 8'b01100010;			//Se está probando el flag N, que el número sea negativo
+		
+		#10
+		//Caso 12
+		A = 8'b00000000;			//C y V deben ser 0
+		B = 8'b00000000;			//Se está probando el flag Z, que el número sea cero
+		
+		#10
+	    //Caso 13
+		OpCode = 'd3;			//AND
+		A = 8'b11011011;			//C y V deben ser 0
+		B = 8'b11101001;			//Se está probando el flag N, que el número sea negativo
+		
+		#10
+		//Caso 14
+	    A = 8'b11111111;			//C y V deben ser 0
+		B = 8'b00000000;			//Se está probando el flag Z, que el número sea cero
+	end
+
+endmodule
